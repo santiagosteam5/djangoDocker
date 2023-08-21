b6f586f865964a5d86efbf8bbc3bcc1a90e905a2
Nicolas Ramirez <nram.vel94@gmail.com>
2023-08-21 07:04:36 -0500

    Added Accounts app, updated base.html

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreateSuccessView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreateSuccessView, CartView, CartRemoveAllView
 
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/product-created', ProductCreateSuccessView.as_view(), name='product-created'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
     
 ]

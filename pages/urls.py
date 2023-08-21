d4f56b4d4ee0f358221fd60becb2b8cd3609a9d9
Nicolas Ramirez <nram.vel94@gmail.com>
2023-08-21 12:32:35 -0500

    Added Media, Interfaces, Utils, Uploaded Images

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreateSuccessView, CartView, CartRemoveAllView
-
+from .views import (HomePageView, AboutPageView, ProductIndexView, 
+                    ProductShowView, ProductCreateView, ProductCreateSuccessView, 
+                    CartView, CartRemoveAllView, ImageViewFactory, ImageViewNoDI
+                    )
+from .utils import ImageLocalStorage
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -11,5 +14,8 @@ urlpatterns = [
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
     path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
-    
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('imagenotdi/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
 ]
